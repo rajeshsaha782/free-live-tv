05f9958a1037c26f4b3a4e78afe001efd8310d01
Rajesh saha <33521669+rajeshsaha782@users.noreply.github.com>
2019-07-07 18:00:26 +0600

    color toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,24 @@ import {DomSanitizer,SafeResourceUrl,} from '@angular/platform-browser';
 export class AppComponent {
 
  public channel:SafeResourceUrl;
+ public fcolor = this.getRandomColor();
  constructor (public sanitizer:DomSanitizer) {
 }
  ngOnInit() {
   this.channel =  this.sanitizer.bypassSecurityTrustResourceUrl("http://freeiptv.ga/tv2.php?channel=0-9-zeebangla");      
+  this.toggleColor();
+}
+
+toggleColor(){
+  console.log("color chaged!!!")
+  setTimeout(() => {
+    this.fcolor = this.getRandomColor();
+    this.toggleColor();
+  }, 1000);
+}
+getRandomColor() {
+  var color = Math.floor(0x1000000 * Math.random()).toString(16);
+  return '#' + ('000000' + color).slice(-6);
 }
 
 }
